856e3a53d931604cb35945f9e56a2124ea4d70f1
Ryan Raub <ryan.raub@asu.edu>
2014-11-19 14:07:01 -0700

    added csslint to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,14 +26,31 @@ module.exports = function (grunt) {
                 standard: 'Wordpress',
                 ignore: 'header-asu.php'
             }
+        },
+        csslint: {
+          options: {
+            csslintrc: 'coding_standards/.csslintrc'
+          },
+          core: [
+            '*.css'
+          ],
+          assets: [
+            'assets/asu-web-standards/css/*.css',
+            'layouts/*.css'
+          ],
+          layouts: [
+            'layouts/*.css'
+          ]
         }
     });
 
     // These plugins provide necessary tasks
     grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 
     // Default task
     grunt.registerTask('default', [
-        'phpcs']);
+        'phpcs', 
+        'csslint']);
 };
 
