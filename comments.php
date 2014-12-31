58a62c0da1d6801e52c75f2c73c2295d760734cb
Ivan Montiel <idmontie@asu.edu>
2014-12-31 15:56:28 -0700

    basic comment css

--- comments.php
+++ comments.php
@@ -18,62 +18,62 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments" class="comments-area container">
+  <div class="row">
+    <?php // You can start editing here -- including this comment! ?>
 
-  <?php // You can start editing here -- including this comment! ?>
+    <?php if ( have_comments() ) : ?>
+      <h2 class="comments-title">
+        <?php
+          printf(
+              _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'asu-wordpress-web-standards-theme' ),
+              number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' 
+          );
+        ?>
+      </h2>
 
-  <?php if ( have_comments() ) : ?>
-    <h2 class="comments-title">
-      <?php
-        printf(
-            _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'asu-wordpress-web-standards-theme' ),
-            number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' 
-        );
+      <?php 
+        if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : 
+          // are there comments to navigate through 
       ?>
-    </h2>
+      <nav id="comment-nav-above" class="comment-navigation" role="navigation">
+        <h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'asu-wordpress-web-standards-theme' ); ?></h1>
+        <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'asu-wordpress-web-standards-theme' ) ); ?></div>
+        <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'asu-wordpress-web-standards-theme' ) ); ?></div>
+      </nav><!-- #comment-nav-above -->
+      <?php endif; // check for comment navigation ?>
 
-    <?php 
-      if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : 
-        // are there comments to navigate through 
-    ?>
-    <nav id="comment-nav-above" class="comment-navigation" role="navigation">
-      <h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'asu-wordpress-web-standards-theme' ); ?></h1>
-      <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'asu-wordpress-web-standards-theme' ) ); ?></div>
-      <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'asu-wordpress-web-standards-theme' ) ); ?></div>
-    </nav><!-- #comment-nav-above -->
-    <?php endif; // check for comment navigation ?>
+      <ol class="comment-list">
+        <?php
+          wp_list_comments(
+              array(
+                'style'      => 'ol',
+                'short_ping' => true,
+              )
+          );
+        ?>
+      </ol><!-- .comment-list -->
 
-    <ol class="comment-list">
       <?php
-        wp_list_comments(
-            array(
-              'style'      => 'ol',
-              'short_ping' => true,
-            )
-        );
+        if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : 
+          // are there comments to navigate through 
       ?>
-    </ol><!-- .comment-list -->
+      <nav id="comment-nav-below" class="comment-navigation" role="navigation">
+        <h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'asu-wordpress-web-standards-theme' ); ?></h1>
+        <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'asu-wordpress-web-standards-theme' ) ); ?></div>
+        <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'asu-wordpress-web-standards-theme' ) ); ?></div>
+      </nav><!-- #comment-nav-below -->
+      <?php endif; // check for comment navigation ?>
+
+    <?php endif; // have_comments() ?>
 
     <?php
-      if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : 
-        // are there comments to navigate through 
+      // If comments are closed and there are comments, let's leave a little note, shall we?
+      if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
     ?>
-    <nav id="comment-nav-below" class="comment-navigation" role="navigation">
-      <h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'asu-wordpress-web-standards-theme' ); ?></h1>
-      <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'asu-wordpress-web-standards-theme' ) ); ?></div>
-      <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'asu-wordpress-web-standards-theme' ) ); ?></div>
-    </nav><!-- #comment-nav-below -->
-    <?php endif; // check for comment navigation ?>
+      <p class="no-comments"><?php _e( 'Comments are closed.', 'asu-wordpress-web-standards-theme' ); ?></p>
+    <?php endif; ?>
 
-  <?php endif; // have_comments() ?>
-
-  <?php
-    // If comments are closed and there are comments, let's leave a little note, shall we?
-    if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-  ?>
-    <p class="no-comments"><?php _e( 'Comments are closed.', 'asu-wordpress-web-standards-theme' ); ?></p>
-  <?php endif; ?>
-
-  <?php comment_form(); ?>
-
+    <?php comment_form(); ?>
+  </div>
 </div><!-- #comments -->
