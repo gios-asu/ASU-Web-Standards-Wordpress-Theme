2390e8db5a3aeef58fbf25b51523c74b9be46a8b
Ivan Montiel <idmontie@asu.edu>
2015-01-21 15:57:12 -0700

    Jobs at asu link. closes #41.

--- footer.php
+++ footer.php
@@ -249,11 +249,12 @@
         <div class="row">
           <div class="col-md-12">
             <ul class="little-foot-nav">
-              <li><a class="" href="http://www.asu.edu/copyright/">Copyright &amp; Trademark</a></li>
-              <li><a class="" href="http://www.asu.edu/accessibility/">Accessibility</a></li>
-              <li><a class="" href="http://www.asu.edu/privacy/">Privacy</a></li>
-              <li><a class="" href="https://cfo.asu.edu/emergency">Emergency</a></li>
-              <li><a class="" href="https://contact.asu.edu/">Contact ASU</a></li>
+              <li><a href="http://www.asu.edu/copyright/">Copyright &amp; Trademark</a></li>
+              <li><a href="http://www.asu.edu/accessibility/">Accessibility</a></li>
+              <li><a href="http://www.asu.edu/privacy/">Privacy</a></li>
+              <li><a href="http://www.asu.edu/asujobs">Jobs At ASU</a></li>
+              <li><a href="https://cfo.asu.edu/emergency">Emergency</a></li>
+              <li><a href="https://contact.asu.edu/">Contact ASU</a></li>
             </ul>
           </div>
         </div><!-- /.row -->
