382f5b1a6dd6991e348460d1d6094d141562f551
Ivan Montiel <idmontie@asu.edu>
2015-03-11 17:16:56 -0700

    add some container tests.

--- tests/ShortcodesTest.php
+++ tests/ShortcodesTest.php
@@ -4,10 +4,21 @@ class ShortcodesTest extends WP_UnitTestCase {
   function test_shortcode_container_creates_containers() {
     $container = do_shortcode('[container][/container]');
 
-    $this->assertEquals('<div class="container  pad-bot-md  pad-top-sm "></div>', $container);
+    $this->assertContains('<div class="container', $container);
+    $this->assertContains('pad-bot-md', $container);
+    $this->assertContains('pad-top-sm', $container);
+    $this->assertContains('</div>', $container);
   }
 
   function test_shortcode_container_creates_gray_containers() {
-    // TODO
+    $container = do_shortcode('[container type=gray][/container]');
+
+    $this->assertContains('<div class="gray-back', $container);
+    $this->assertContains('<div class="container', $container);
+    $this->assertContains('pad-bot-md', $container);
+    $this->assertContains('pad-top-md', $container);
+    $this->assertContains('</div>', $container);
   }
+
+  
 }
