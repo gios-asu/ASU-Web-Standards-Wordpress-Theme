ea5228c3efe74742bf19f5cbb00bb142487ed838
Nagarjuna Myla <chasethenag420@gmail.com>
2015-04-17 17:04:31 -0700

    refactoring the shorcode.php to different files under shortcodes dir

--- functions.php
+++ functions.php
@@ -240,7 +240,14 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Shortcodes.
  */
-require get_template_directory() . '/inc/shortcodes.php';
+require get_template_directory() . '/inc/shortcodes/breadcrumbs-shortcode.php';
+require get_template_directory() . '/inc/shortcodes/buttons-shortcode.php';
+require get_template_directory() . '/inc/shortcodes/columns-shortcode.php';
+require get_template_directory() . '/inc/shortcodes/containers-shortcode.php';
+require get_template_directory() . '/inc/shortcodes/page-features-shortcode.php';
+require get_template_directory() . '/inc/shortcodes/panels-shortcode.php';
+require get_template_directory() . '/inc/shortcodes/related-links-shortcode.php';
+require get_template_directory() . '/inc/shortcodes/sidebar-nav-shortcode.php';
 
 /**
  * Filters
