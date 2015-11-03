c08e6781945cce993cda1f98efa1b4dc61ac3299
Ryan Raub <ryan.raub@asu.edu>
2015-11-02 17:06:34 -0700

    added better alt text to footer innovation badge

--- footer.php
+++ footer.php
@@ -329,7 +329,7 @@ if ( is_array( get_option( 'wordpress_asu_theme_options' ) ) ) {
           <div class="col-md-12">
             <div class="little-foot__innovate-image-wrapper pull-left">
                <a href="http://yourfuture.asu.edu/rankings" target="_blank" id='best-colleges-us-news-bage-icon'>
-                <img src="<?php echo get_template_directory_uri() ?>/assets/images/best-colleges-us-news-badge.png"  alt="mostInnovative">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/images/best-colleges-us-news-badge.png"  alt="Best Colleges U.S. News Most Innovative 2016">
               </a>
             </div>
             <div class="little-foot__right">
