989f333554cee1de510aa8e797487988aae23c0f
Ivan Montiel <idmontie@asu.edu>
2015-12-04 10:50:10 -0700

    New version of web standards, close #196

--- assets/asu-web-standards/js/bootstrap-asu.min.js
+++ assets/asu-web-standards/js/bootstrap-asu.min.js
@@ -1,7 +1,7 @@
 /**========================================================================
- * Web Standards: bootstrap-asu.js v0.1.9
+ * Web Standards: bootstrap-asu.js v0.2.2
  * ========================================================================
- * Copyright 2014 ASU
+ * Copyright 2014-2015 ASU
  * Licensed under MIT (https://github.com/gios-asu/ASU-Bootstrap-Addon/blob/master/LICENSE)
  * @license MIT
  * ======================================================================== */
