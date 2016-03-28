06a6671bd6047f863770021081f3c5985ac7f0da
Ryan Raub <ryan.raub@asu.edu>
2016-03-28 11:47:01 -0700

    whitespace

--- sidebar.php
+++ sidebar.php
@@ -6,14 +6,14 @@
  */
 ?>
 <?php
-	if ( is_active_sidebar( 'footer' ) ) :
-    dynamic_sidebar( 'footer' );
-	endif;
+if ( is_active_sidebar( 'footer' ) ) :
+  dynamic_sidebar( 'footer' );
+endif;
 
-  if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-    <div id="sidebarNav" class="sidebar-nav affix-top">
-      <?php dynamic_sidebar( 'sidebar-1' ); ?>
-    </div>
-  <?php 
-  endif;
-	?>
+if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+  <div id="sidebarNav" class="sidebar-nav affix-top">
+    <?php dynamic_sidebar( 'sidebar-1' ); ?>
+  </div>
+<?php
+endif;
+?>
