b39bdeb3ca635617fcc23e639a8301e70e5d920a
Ryan Raub <ryan.raub@asu.edu>
2016-09-29 10:05:49 -0700

    factored our the analytics tracking codes into their own file for easier child theming

--- header.php
+++ header.php
@@ -176,43 +176,7 @@ HTML;
 <body <?php body_class(); ?>>
   <a href="#skippy" class="sr-only">Skip to Content</a>
 
-  <!-- Google Tag Manager ASU Universal-->
-  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KDWN8Z"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','SI_dataLayer','GTM-KDWN8Z');</script>
-  <!-- End Google Tag Manager ASU Universal -->
-  <!-- Lavidge AdWords Tracking -->
-  <script>
-    var versaTag = {};
-    versaTag.id = "5381";
-    versaTag.sync = 0;
-    versaTag.dispType = "js";
-    versaTag.ptcl = "HTTPS";
-    versaTag.bsUrl = "bs.serving-sys.com/BurstingPipe";
-    versaTag.activityParams = {
-    "OrderID":"","Session":"","Value":"","productid":"","productinfo":"","Quantity":""
-    };
-    versaTag.retargetParams = {};
-    versaTag.dynamicRetargetParams = {};
-    versaTag.conditionalParams = {};
-  </script>
-  <script id="ebOneTagUrlId" src="https://secure-ds.serving-sys.com/SemiCachedScripts/ebOneTag.js"></script>
-  <noscript>
-    <iframe src="https://bs.serving-sys.com/BurstingPipe?
-      cn=ot&amp;
-      onetagid=5381&amp;
-      ns=1&amp;
-      activityValues=$$Value=[Value]&amp;OrderID=[OrderID]&amp;Session=[Session]&amp;ProductID=[ProductID]&amp;ProductInfo=[ProductInfo]&amp;Quantity=[Quantity]$$&amp;
-      retargetingValues=$$$$&amp;
-      dynamicRetargetingValues=$$$$&amp;
-      acp=$$$$&amp;"
-      style="display:none;width:0px;height:0px"></iframe>
-  </noscript>
-  <!-- End Lavidge AdWords Tracking -->
+  <?php include 'analytics-body-tracking.php'; ?>
 
   <div id="page-wrapper">
     <div id="page">
