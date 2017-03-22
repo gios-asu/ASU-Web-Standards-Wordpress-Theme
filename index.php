09679634ceb76812c1e501125b465e525c9bfdb2
Ryan Raub <ryan.raub@gmail.com>
2017-03-22 10:38:26 -0700

    more typos

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
  * @package asu-wordpress-web-standards
  */
 
-include_template_file( 'helpers/mime-types-helper.php' );
+include_theme_file( 'helpers/mime-types-helper.php' );
 
 get_header();
 
