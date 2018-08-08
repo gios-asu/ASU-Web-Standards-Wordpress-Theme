5bf0a47ad45b544310d435338e4e68a2f1bb1983
Walter McConnell <Walter.McConnell@asu.edu>
2018-08-08 11:55:19 -0700

    Remove Additional-Info Javascript
    * Removed the lines pertaining to dealing with the hidden information area that was removed from the People Pages slim view.
    * Changed the drop-down code to activate on change, and no on click, because it works better on mobile (I think)

--- assets/js/people-pages.js
+++ assets/js/people-pages.js
@@ -6,29 +6,8 @@
 
   $( document ).ready( function() {
 
-    // Hides the disclosure triangle when a person record has no additional details
-    $('.additional-info').each( function( index ) {
-      if( $.trim( $(this).text() ).length == 0) {
-        $(this).siblings('.show-details').css('display', 'none');
-      }
-    });
-
-    // Toggles the additional details section when the disclosure triangle is clicked
-    $('.show-details').click( function() {
-
-      $( this ).siblings( '.additional-info' ).slideToggle();
-
-      if( $(this).hasClass( 'fa-caret-left' )) {
-        $( this ).removeClass( 'fa-caret-left' );
-        $( this ).addClass( 'fa-caret-down' );
-      }else{
-        $( this ).removeClass( 'fa-caret-down' );
-        $( this ).addClass( 'fa-caret-left' );
-      }
-    });
-
     // Jumps to a named anchor when selected in the dropdown (mobile only)
-    $( '#faculty-dropdown' ).click( function( e ) {
+    $( '#faculty-dropdown' ).change( function( e ) {
       window.location = $( '#faculty-dropdown' ).val();
 
     });
